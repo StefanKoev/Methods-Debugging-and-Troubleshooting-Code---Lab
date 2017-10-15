047fdb9d743278096cad5d0bbb5b5f3be1d947a1
Stefan Koev <koev.stefan@gmail.com>
2017-10-16 00:01:08 +0300

    Create 07. Math Power

---
+++ 07.Math Power/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Math_Power
+{
+    class Program
+    {
+        static double Power(double number, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Power(number, power));
+
+        }
+    }
+}
