c25a2080e3ed71da99772125e0e94b498073b238
Stefan Koev <koev.stefan@gmail.com>
2017-10-15 22:45:24 +0300

    Create 05.Temperature Conversion

---
+++ 05.Temperature Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Temperature_Conversion
+{
+    class Program
+    {
+        static double FahrenneitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+        static void Main()
+        {
+            var fahrenheit = double.Parse(Console.ReadLine());
+            var celsius = FahrenneitToCelsius(fahrenheit);
+            Console.WriteLine("{0:F2}", celsius);
+        }
+    }
+}
